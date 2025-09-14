d78906f521e2ad3996c8e68ba10b8d4b25d70418
David Šebík <sebik@shipard.cz>
2025-09-14 10:23:15 +0200

    Lokální záloha obsahuje `zigbee2mqtt`

--- tools/shipard-node.php
+++ tools/shipard-node.php
@@ -160,6 +160,10 @@ class NodeApp extends \Shipard\Application
 		if (is_dir('/opt/shn-exts'))
 			exec ("cd / && tar -Pczf $thisLocalBackupDir/opt-shn-exts-$thisHostName-" . date ('Y-m-d') . ".tgz /opt/shn-exts/");
 
+		// -- /opt/zigbee2mqtt
+		if (is_dir('/opt/zigbee2mqtt'))
+			exec ("cd / && tar -Pczf $thisLocalBackupDir/opt-zigbee2mqtt-$thisHostName-" . date ('Y-m-d') . ".tgz /opt/zigbee2mqtt/");
+
 		// -- /root/.ssh
 		if (is_dir('/root/.ssh'))
 			exec ("cd / && tar -Pczf $thisLocalBackupDir/root-ssh-$thisHostName-" . date ('Y-m-d') . ".tgz /root/.ssh");
